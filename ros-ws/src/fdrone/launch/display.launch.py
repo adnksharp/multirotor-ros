792f79e0479d7d1f459a0758014254ebd3d4560e
Sharp <darkmark404@protonmail.com>
2025-04-09 00:09:38 -0600

    refactor(ros-ws): change pos_pub.py to get_pub.py

--- ros-ws/src/fdrone/launch/display.launch.py
+++ ros-ws/src/fdrone/launch/display.launch.py
@@ -41,7 +41,7 @@ def generate_launch_description() -> LaunchDescription:
 
     jsp_node = Node(
         package='fdrone',
-        executable='pos_pub.py',
+        executable='get_pub.py',
         name='robot_position_publisher',
     )
     ld.add_action(jsp_node)
